f3f71af39409ce4aa6b824f9f3eb7e5b6b4a12fe
Morgan Berry <morganlyn.berry@gmail.com>
2022-10-28 14:29:39 -0600

    Delete extra file and commit small syntax changes

--- src/components/paintingCard/PaintingCard.jsx
+++ src/components/paintingCard/PaintingCard.jsx
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './PaintingCard.css'
 
-const PaintingCard = () => {
+const PaintingCard = ({}) => {
   return (
-      <section className='individual-painting' id={PaintingCard.id}>
+      <section className='individual-painting' id={artObject.label.id}>
         <img className='individual-painting-img' alt={artObject.label.title} src={artObject.webImage.url}/>
         <div className='painting-details'>
-        <h1 className='individual-painting-title'>{artObject.label.description}</h1>
-        <h2 className='painting-author'>{artObject.principalMakers[0].name}</h2>
+        <h1 className='individual-painting-title'>{artObject.title}</h1>
+        <h2 className='individual-painting-author'>{artObject.principalMakers[0].name}</h2>
         <p className='individual-painting-description'>{artObject.label.description}</p>
+        <p className='individual-painting-date'>{artObject.label.date}</p>
         </div>
       </section> 
   )
