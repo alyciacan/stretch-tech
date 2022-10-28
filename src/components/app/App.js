2f2bfa253919d4cd68b7cf654e7d1720f3b77c5b
Morgan Berry <morganlyn.berry@gmail.com>
2022-10-28 17:51:52 -0600

    Add deconstruction to PaintingCard

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Form />
-      <PaintingCard />
-      <IndividualPainting />
+      {/* <PaintingCard /> */}
+      {/* <IndividualPainting /> */}
     </div>
   );
 }
--- src/components/paintingCard/PaintingCard.jsx
+++ src/components/paintingCard/PaintingCard.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './PaintingCard.css'
 
 const PaintingCard = () => {
+  const { id , title, description, date } = artObject.label
+  const { url } = artObject.webImage
 
-  // return (
-  //   <div className='painting-card' id={} >
-  //   <img className='card-imgs' src={} alt={} />
-  //   <h3 className='card-titles'>{}</h3>
-  //   </div>
-  // )
+  return (
+    <div className='painting-card' id={} >
+    <img className='card-imgs' src={url} alt={} />
+    <h3 className='card-titles'>{}</h3>
+    </div>
+  )
 }
 
 export default PaintingCard
