65229b6e525a4458355c2d0598309795ab8cd907
Alycia Canavan <alycia.canavan@gmail.com>
2022-10-28 09:01:38 -0700

    Add api calls for single painting and all art

---
+++ src/apiCalls.js
@@ -0,0 +1,20 @@
+const checkResponse = (response) => {
+    if(!Response.ok) {
+        throw new Error(
+            `Error: ${response.status}`
+        )
+    }
+    return response.json();
+};
+
+const getAllArt = () => {
+    return fetch('https://www.rijksmuseum.nl/api/en/collection?key=AgQXh8Og&involvedMaker=Rembrandt+van+Rijn')
+        .then(response => checkResponse(response))
+};
+
+const getSinglePainting = (id) => {
+    return fetch(`https://www.rijksmuseum.nl/api/en/collection/${id}?key=AgQXh8Og`)
+        .then(response => checkResponse(response))
+};
+
+export { getArt, getSinglePainting};
