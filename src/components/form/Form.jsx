606b9f99f75141ce9a600116e08abba6b0920a7b
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-10-28 10:49:00 -0600

    Add: form with input and submit button

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,16 @@
+
+import "./Form.css"
+
+const Form = () => {
+ 
+
+  return (
+    <form className="form-container">
+      <input 
+      />
+      <button >Save Meme</button>
+    </form>
+  );
+};
+
+export default Form;
