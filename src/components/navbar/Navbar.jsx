8744eb00d17f32dbe331a11a8aca449adae33996
Alycia Canavan <alycia.canavan@gmail.com>
2022-10-28 21:45:21 -0700

    Add styling for navbar and logo image.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import './Navbar.css';
 import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import logo from './artparty-logo.png';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <Router>
-                <Link to='/' >
-                    <img src='' alt="Art Party logo"/>
+        <Router>
+            <nav className="navbar">
+                <Link to='/' className="logo-link">
+                    <img src={ logo } alt="Art Party logo"/>
                 </Link>
                 <NavLink to="/gallery" className="link">my gallery</NavLink>
-            </Router>
-        </nav>
+            </nav>
+        </Router>
     )
 }
 
