bef851a3a83052c241ebf3231e88ae9019861ede
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-10-31 18:22:07 -0600

    Fix: home route removed Router that only need to be down once in the index.js

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,15 @@
 import './Navbar.css';
-import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from './artparty-logo.png';
 
 const Navbar = () => {
     return (
-        <Router>
             <nav className="navbar">
-                <Link to='/' className="navbar-link">
+                <Link className="navbar-link" to='/' >
                     <img src={ logo } alt="Art Party logo" className="logo-img"/>
                 </Link>
                 <NavLink to="/gallery" className="navbar-link">my gallery</NavLink>
             </nav>
-        </Router>
     )
 }
 
