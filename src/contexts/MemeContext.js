a6329927c31b1bb7a0a3f40a05df8ee9a3455541
Alycia Canavan <alycia.canavan@gmail.com>
2022-11-01 14:59:27 -0700

    Add code to MemeContext

--- src/contexts/MemeContext.js
+++ src/contexts/MemeContext.js
@@ -2,8 +2,15 @@ import { createContext, useState } from 'react';
 
 const MemeContext = createContext();
 
-export const MemeProvider = ({ img, memeText }) => {
+export const MemeProvider = ({ children }) => {
+    const [memes, setMemes] = useState([]);
+    const value = {memes, setMemes}
 
+    return (
+        <MemeContext.Provider value={ value }>
+            { children }
+        </MemeContext.Provider>
+    )
 }
 
 export default MemeContext;
