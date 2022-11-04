20fddf5b4ed34b2ba8e542e0219e4653a636e68d
Alycia Canavan <alycia.canavan@gmail.com>
2022-11-03 21:12:45 -0700

    Add padding, background color to MyGallery

--- cypress/e2e/myGallery_spec.cy.js
+++ cypress/e2e/myGallery_spec.cy.js
@@ -1,5 +1,17 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('MyGallery component', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/mygallery')
+  });
+
+  it('shows the user the navbar at the top', () => {
+    cy.get('.navbar').should('be.visible');
+  });
+
+  it('starts out without any memes', () => {
+    cy.get('.my-gallery').children().should('not.exist');
   })
+
+  it(''
+  )
+
 })
