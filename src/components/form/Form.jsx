9e562cd2d9e0b4684e444322a1053db499a8f025
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-11-04 09:19:35 -0600

    Ref: button moved to Form componenet

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,37 +1,58 @@
 import React, { useState } from "react";
-import "./Form.css"
+import "./Form.css";
 
-const Form = ({getMemeTitle}) => {
+const Form = ({ getMemeTitle, saveMeme }) => {
   const [sentence, setSentence] = useState("");
 
   const handleChange = (event) => {
     setSentence(event.currentTarget.value);
-    getMemeTitle(event.currentTarget.value)
+    getMemeTitle(event.currentTarget.value);
   };
 
   const handleMax = () => {
-    const maxLength = 50 - sentence.length
-    return maxLength
+    const maxLength = 50 - sentence.length;
+    return maxLength;
   };
 
   const handleColor = () => {
-    if(handleMax() < 20){
-        return ({
-            color: "red"
-        })
+    if (handleMax() < 20) {
+      return {
+        color: "red",
+      };
     }
   };
- 
+
+  const clearForm = () => {
+    saveMeme();
+    setSentence("");
+  };
+
   return (
-    <form>
-      <input maxLength={50} className="meme-input"
-        value={sentence}
-        type="text"
-        placeholder="Enter text here"
-        onChange={(event) => handleChange(event)}
-      />
-      {sentence.length > 0 && <span style={handleColor()}> Remaining Characters: {handleMax()} </span> }
-     </form>
+    <section className="container">
+      <form className="form-container">
+        <input
+          maxLength={50}
+          className="meme-input"
+          value={sentence}
+          type="text"
+          placeholder="Enter text here"
+          onChange={(event) => handleChange(event)}
+        />
+        {sentence.length > 0 && (
+          <span style={handleColor()}>
+            Remaining Characters: {handleMax()}
+          </span>
+        )}
+      </form>
+        <button
+          disabled={!sentence}
+          className="meme-save-button"
+          type="button"
+          onClick={() => clearForm()}
+        >
+          Save Meme
+        </button>
+    </section>
   );
 };
 
