67ba8428384123e00ca7f6614f3ce7eeea0d196e
Alycia Canavan <alycia.canavan@gmail.com>
2022-11-03 23:26:50 -0700

    begin testing for MyGallery

--- cypress/e2e/myGallery_spec.cy.js
+++ cypress/e2e/myGallery_spec.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('MyGallery component', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/IndividualPainting/SK-C-6').as('indivPainting')
+    cy.wait('@indivPainting')
+    cy.get('.meme-input').type("This is a test meme!")
+    cy.get('.meme-save-button').click()
+    cy.get('.my-gallery-link').click()
+  })
+  it('should have the correct url: /mygallery', () => {
+    cy.url().should('include', '/mygallery')})
+  
+  it('displays the memes created by the user', () => {
+    cy.get('.meme-card').contains('This is a test meme!')
   })
 })
