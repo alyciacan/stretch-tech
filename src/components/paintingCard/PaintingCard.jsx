8237c9cf0d521241cfcf92555ad1082dee89d410
Morgan Berry <morganlyn.berry@gmail.com>
2022-11-07 08:21:11 -0700

    Add alt text to img

--- src/components/paintingCard/PaintingCard.jsx
+++ src/components/paintingCard/PaintingCard.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 const PaintingCard = ({url, name, id, memeText}) => {
     return (
       <Link to={`/IndividualPainting/${id}`} className='cards-container'>
-        <img className='images' id={id} src={url} />
+        <img className='images' alt={'Artists Painting'} id={id} src={url} />
         <p className='card-titles'>{name}</p>
         <p className="memeTitle">{memeText}</p>
       </Link>
