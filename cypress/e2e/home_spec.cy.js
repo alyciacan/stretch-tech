2d648960c872a944befb68d0ff9bc3dc89c11e11
Morgan Berry <morganlyn.berry@gmail.com>
2022-11-07 12:26:15 -0700

    Add syntax fixes to tests

--- cypress/e2e/home_spec.cy.js
+++ cypress/e2e/home_spec.cy.js
@@ -7,7 +7,7 @@ describe('As a user, when I load the application, I can see a collection of pain
     })
   })
 
-  it.only('should display an error message if paintings are unable to be displayed on the screen', () => {
+  it('should display an error message if paintings are unable to be displayed on the screen', () => {
     cy.intercept(
       "GET",
       "https://www.rijksmuseum.nl/api/en/collection?key=AgQXh8Og&involvedMaker=Rembrandt+van+Rijn",
@@ -35,68 +35,68 @@ describe('As a user, when I load the application, I can see a collection of pain
   
   it('should display the navBar with application logo and buttons', () => {
     cy
-    .get('.logo-img').should('be.visible')
-    .get('.navbar-link').should('be.visible')
+      .get('.logo-img').should('be.visible')
+      .get('.navbar-link').should('be.visible')
   })
   
   it('should be able to click on the myGallery button and be taken to myGallery', () => {
     cy
-    .get('.my-gallery-link')
-    .click()
+      .get('.my-gallery-link')
+      .click()
   })
   
   it('should display the aside that read "Art Gallery" and a p tag with directions for the site', () => {
     cy
-    .get('h2').contains('Art Party')
-    .get('.description > p').contains('is how the most sophisticated, discerning art history buffs get their meme jollies. Simply click on your favorite oeuvre to get started!')
+      .get('h2').contains('Art Party')
+      .get('.description > p').contains('is how the most sophisticated, discerning art history buffs get their meme jollies. Simply click on your favorite oeuvre to get started!')
   })
   
   it('should display all paintings', () => {
     cy
-    .get('.images-container').should('exist')
-    .get(':nth-child(1) > .images').should('be.visible')
-    .get(':nth-child(2) > .images').should('be.visible')
+      .get('.images-container').should('exist')
+      .get(':nth-child(1) > .images').should('be.visible')
+      .get(':nth-child(2) > .images').should('be.visible')
   })
   
   it('should be able to click on a painting a view details about it', () => {
     cy
-    .get('.images-container').should('exist')
-    .get(':nth-child(1) > .images').should('be.visible')
-    .get('#SK-A-5033').click()
-    .get('.images-container').should('not.exist')
-    .url().should('eq', 'http://localhost:3000/IndividualPainting/SK-A-5033')
+      .get('.images-container').should('exist')
+      .get(':nth-child(1) > .images').should('be.visible')
+      .get('#SK-A-5033').click()
+      .get('.images-container').should('not.exist')
+      .url().should('eq', 'http://localhost:3000/IndividualPainting/SK-A-5033')
   })
     
-    it('should not display details for an individual painting', () => {
-      cy
+  it('should not display details for an individual painting', () => {
+    cy
       .url().should('eq', 'http://localhost:3000/')
       .url().should('not.eq', 'http://localhost:3000/IndividualPainting/SK-A-4050')
       .get('.individual-painting').should('not.exist')
-    })
+  })
     
-    it('Should be able to use the browser arrow buttons to go between the home page and individual painting page', () => {
-      cy.intercept(
-        'GET', 
-        'https://www.rijksmuseum.nl/api/en/collection/**', 
-        { fixture: 'painting2.json'}
-        )
-        cy.visit("http://localhost:3000/IndividualPainting/SK-A-4050")
+  it('Should be able to use the browser arrow buttons to go between the home page and individual painting page', () => {
+    cy.intercept(
+      'GET', 
+      'https://www.rijksmuseum.nl/api/en/collection/**', 
+      { fixture: 'painting2.json'}
+      )
+      cy.visit("http://localhost:3000/IndividualPainting/SK-A-4050")
         .url().should('eq', 'http://localhost:3000/IndividualPainting/SK-A-4050')
         .go('back')
         .url().should('eq', 'http://localhost:3000/')
         .go('forward')
         .url().should('eq', 'http://localhost:3000/IndividualPainting/SK-A-4050')
-      }) 
+    }) 
 
-    it('should display a page loading message while waiting for a paintings to display on page', () => {
-      cy
+  it('should display a page loading message while waiting for a paintings to display on page', () => {
+    cy
       .get('.images-container').should('exist')
       .get(':nth-child(1) > .images').should('be.visible')
       .get('#SK-C-5').click()
       .intercept("GET", "https://www.rijksmuseum.nl/api/en/collection/#SK-C-5?key=AgQXh8Og", {fixture: "painting1.json"}).as("painting1");
       cy.get('.loading-text').should('exist').contains('Loading...')
-      cy.get('.loading-text').should('not.exist')
-  
-    })
+      .get('.loading-text').should('not.exist')
+
   })
+})
     
