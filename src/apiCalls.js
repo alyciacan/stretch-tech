725b74706ba7e2dc6b722b75b24458d42c63cf5d
Alycia Canavan <alycia.canavan@gmail.com>
2022-11-18 12:59:43 -0800

    update apicall url so more paintings are fetched

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,7 +8,7 @@ const checkResponse = (response) => {
 	} 
 
 const getAllArt = () => {
-	return fetch(`https://www.rijksmuseum.nl/api/en/collection?key=${API_key}&ps=50&type=painting`)
+	return fetch(`https://www.rijksmuseum.nl/api/en/collection?key=${API_key}&ps=75&type=painting`)
 		.then(response => checkResponse(response))
 };
 
