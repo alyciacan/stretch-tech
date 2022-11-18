8ecbedb593658baeaac23cce26d508283a99ffae
Jedeo <jedeopastor@gmail.com>
2022-11-18 13:45:02 -0700

    Fix: stylysheet

--- src/components/individualPainting/IndividualPainting.jsx
+++ src/components/individualPainting/IndividualPainting.jsx
@@ -10,24 +10,24 @@ import home from "../../images/home.png";
 const IndividualPainting = ({ selectedId }) => {
   const [paintingObject, setPaintingObject] = useState({});
   const [memeTitle, setMemeTitle] = useState("");
-  const [textColor, setTextColor] = useState({ color: 'white' })
+  const [textColor, setTextColor] = useState({ color: "white" });
   const error = useHistory();
-  
+
   useEffect(() => {
     const findPainting = () => {
       getSinglePainting(selectedId)
-      .then((response) => {
-        const obj = {
-          id: response.artObject.id,
-          title: response.artObject.title,
-          description: response.artObject.label.description,
-          img: response.artObject.webImage.url,
-          artist: response.artObject.principalMakers[0].name,
-          year: response.artObject.dating.presentingDate,
-        };
-        setPaintingObject(obj);
-      })
-      .catch(() => error.push('/error'))
+        .then((response) => {
+          const obj = {
+            id: response.artObject.id,
+            title: response.artObject.title,
+            description: response.artObject.label.description,
+            img: response.artObject.webImage.url,
+            artist: response.artObject.principalMakers[0].name,
+            year: response.artObject.dating.presentingDate,
+          };
+          setPaintingObject(obj);
+        })
+        .catch(() => error.push("/error"));
     };
     findPainting();
   }, []);
@@ -44,35 +44,40 @@ const IndividualPainting = ({ selectedId }) => {
     setMemeTitle("");
   };
 
-  if(!id) {
-    return (
-    <h3 className='loading-text'>Loading...</h3>
-    )
+  if (!id) {
+    return <h3 className="loading-text">Loading...</h3>;
   } else {
-      return (
-        <section className="individual-painting">
-          <React.Fragment>
-            <Link to="/" className="home-button" >
-                <img src={home} alt="Home button" role="button"/>
-            </Link>
-            <div className="panting-title-container">
+    return (
+      <section className="individual-painting">
+        <React.Fragment>
+          <Link to="/" className="home-button">
+            <img src={home} alt="Home button" role="button" />
+          </Link>
+          <div className="panting-title-container">
             <img className="individual-painting-img" src={img} alt={title} />
-            <p className="meme-title" style={textColor}>{memeTitle}</p>
+            <p className="meme-title" style={textColor}>
+              {memeTitle}
+            </p>
+          </div>
+          <div className=".painting-details">
+            <h1 className="individual-painting-title">{title}</h1>
+            <h2 className="individual-painting-artist">
+              Artist: {artist} | {year}
+            </h2>
+            <p className="individual-painting-description">{description}</p>
+            <div className="meme-form">
+              <Form
+                saveMeme={saveMeme}
+                getMemeTitle={getMemeTitle}
+                setTextColor={setTextColor}
+                textColor={textColor}
+              />
             </div>
-            <div className=".painting-details">
-              <h1 className="individual-painting-title">{title}</h1>
-              <h2 className="individual-painting-artist">
-                Artist: {artist} | {year}
-              </h2>
-              <p className="individual-painting-description">{description}</p>
-              <div className="meme-form">
-                <Form saveMeme={saveMeme} getMemeTitle={getMemeTitle} setTextColor={setTextColor} textColor={textColor}/>
-              </div>
-            </div>
-          </React.Fragment>
-        </section>
-      );
-    }
+          </div>
+        </React.Fragment>
+      </section>
+    );
+  }
 };
 
 export default IndividualPainting;
